dd86cf49522b758b2414e97869450aa043111fed
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-10-02 17:17:20 +0300

    The special "children" Prop

--- reactEssentials/src/App.jsx
+++ reactEssentials/src/App.jsx
@@ -4,6 +4,7 @@ import { CORE_CONCEPTS } from "./data.js";
 
 import Header from "./components/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -18,6 +19,15 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[2]} />
             <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
---
+++ reactEssentials/src/components/TabButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TabButton({ children }) {
+  return (
+    <li>
+      <button>{children}</button>
+    </li>
+  );
+}
