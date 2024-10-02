3f32b4bfdb1d4ade9a9c5cc14ab085b0ede01cb6
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-10-02 15:21:49 +0300

    Making Components Reusable with Props

--- reactEssentials/src/App.jsx
+++ reactEssentials/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import imgCoreConcepts from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
+import configImg from "./assets/config.png";
+import jsxImg from "./assets/jsx-ui.png";
 
 const reactDescribes = ["Fundamental", "Crucial", "Core"];
 
@@ -7,20 +10,59 @@ function getRandom(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function App() {
+function Header() {
   const describe = reactDescribes[getRandom(2)];
   return (
+    <header>
+      <img src={imgCoreConcepts} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {describe} React concepts you will need for almost any app you are going
+        to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+function App() {
+  return (
     <div>
-      <header>
-        <img src={imgCoreConcepts} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {describe} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcepts
+              img={componentImg}
+              title="components"
+              description="The core UI building"
+            />
+            <CoreConcepts
+              img={configImg}
+              title="components"
+              description="The core UI building"
+            />
+            <CoreConcepts
+              img={jsxImg}
+              title="components"
+              description="The core UI building"
+            />
+            <CoreConcepts
+              img={imgCoreConcepts}
+              title="components"
+              description="The core UI building"
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
