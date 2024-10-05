555c88693438e2291ad8b8d86d02f05b7593531d
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-10-05 23:06:52 +0300

    Update folder by Icons

--- pop-up-store/src/components/Editor/Editor.tsx
+++ pop-up-store/src/components/Editor/Editor.tsx
@@ -1,13 +1,59 @@
 import React from "react";
 import "./Editor.scss";
+
+import SubjectIcon from "@mui/icons-material/Subject";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
+import SegmentIcon from "@mui/icons-material/Segment";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatStrikethroughIcon from "@mui/icons-material/FormatStrikethrough";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+import SuperscriptIcon from "@mui/icons-material/Superscript";
+import SubscriptIcon from "@mui/icons-material/Subscript";
+
+import Dropdown from "./subComponents/Dropdown/Dropdown";
+import Link from "./subComponents/Link/Link";
 const Editor = () => {
   return (
     <div className="editor-main">
       <div className="editor-header">
-        <div className="button-container-1">x</div>
-        <div className="button-container-2">y</div>
-        <div className="button-container-3">z</div>
-        <div className="button-container-4">w</div>
+        <div className="button-container-1">
+          <button>
+            <SubjectIcon />
+          </button>
+          <button>
+            <FormatAlignJustifyIcon />
+          </button>
+          <button>
+            <SegmentIcon />
+          </button>
+        </div>
+        <div className="button-container-2">
+          <Dropdown />
+          <Link />
+        </div>
+        <div className="button-container-3">
+          <button>
+            <FormatBoldIcon />
+          </button>
+          <button>
+            <FormatItalicIcon />
+          </button>
+          <button>
+            <FormatStrikethroughIcon />
+          </button>
+          <button>
+            <FormatUnderlinedIcon />
+          </button>
+        </div>
+        <div className="button-container-4">
+          <button>
+            <SuperscriptIcon />
+          </button>
+          <button>
+            <SubscriptIcon />
+          </button>
+        </div>
       </div>
       <div className="editor-text" contentEditable="true">
         text
