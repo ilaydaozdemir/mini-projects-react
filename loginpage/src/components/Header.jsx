5a405a56c3d51279f2ade51e206de2f9d9b35f1b
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-10-31 17:01:33 +0300

    Add css module

--- loginpage/src/components/Header.jsx
+++ loginpage/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import logo from '../assets/logo.png';
-
+import logo from "../assets/logo.png";
+import classes from "../cssModules/Header.module.css";
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={classes.text}>A community of artists and art-lovers.</p>
     </header>
   );
 }
