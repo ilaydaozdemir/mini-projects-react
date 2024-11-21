e5220bdb43d74032e31c310519b5f98d23620b33
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-11-21 21:05:21 +0300

    Handling Project Creation & Updating the UI

--- projectmanagment/src/App.jsx
+++ projectmanagment/src/App.jsx
@@ -26,11 +26,12 @@ function App() {
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-  console.log(projectState);
+
   let content;
 
   if (projectState.selectedProjectId === null) {
@@ -40,7 +41,10 @@ function App() {
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
