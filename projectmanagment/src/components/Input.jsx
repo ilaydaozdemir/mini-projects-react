d11cd660c6bb510fa4a1371bd13a7f4b08ad084a
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-11-21 16:45:38 +0300

    Styling by tailwind css

--- projectmanagment/src/components/Input.jsx
+++ projectmanagment/src/components/Input.jsx
@@ -1,8 +1,16 @@
 function Input({ textArea, label, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
   return (
-    <p>
-      <label>{label}</label>
-      {textArea ? <textarea {...props} /> : <input />}
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textArea ? (
+        <textarea {...props} className={classes} />
+      ) : (
+        <input className={classes} />
+      )}
     </p>
   );
 }
